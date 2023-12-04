220c16cd83c382d654ef5c4156be320c5b7f4f7c
Xavier Garay <xaviergaray0010@gmail.com>
2023-12-04 14:17:28 -0500

    added navbar

--- alpha-corsi-website/components/layout.js
+++ alpha-corsi-website/components/layout.js
@@ -7,7 +7,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = '[Your Name]';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'AlphaCorsi Security';
 
 export default function Layout({ children, home }) {
   return (
@@ -35,37 +35,18 @@ export default function Layout({ children, home }) {
         }
       />
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt={name}
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
+        <nav className={styles.navbar}>
+            <div className={styles.leftNav}>
+                <Link href='/' className={styles.alphacorsiNav}>ALPHACORSI</Link>
+                <Link href='/#services' className={styles.otherNav}>Services</Link>
+                <Link href='/#about' className={styles.otherNav}>About</Link>
+                <Link href='/#follow-us' className={styles.otherNav}>Follow Us</Link>
+            </div>
+
+            <div className={styles.rightNavButton}>
+                <Link href='/#contact-us' className={styles.contactUsNav}>Contact Us</Link>
+            </div>
+        </nav>
       </header>
       <main>{children}</main>
       {!home && (
