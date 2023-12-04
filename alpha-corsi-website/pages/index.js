19da56a33159df4aec53b40a9325cc9ba94d24ab
Xavier Garay <xaviergaray0010@gmail.com>
2023-12-04 14:53:31 -0500

    Added all content for hero section

--- alpha-corsi-website/pages/index.js
+++ alpha-corsi-website/pages/index.js
@@ -14,27 +14,14 @@ export default function Home({ allPostsData }) {
         <video autoPlay muted loop className={utilStyles.video}>
             <source src="/heroMountainBackground.mp4" type="video/mp4"/>
         </video>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+
+        <div className={utilStyles.hero}>
+          <h1>AlphaCorsi</h1>
+          <p>A boutique Information Security consultancy firm</p>
+          <div className={utilStyles.learnmoreButton}>
+            <Link href='/#learn-more' className={utilStyles.learnmoreText}>Learn More</Link>
+          </div>
+        </div>
     </Layout>
   );
 }
