8bb1924d071020a6f2030292e1e0d892e94193a0
Xavier Garay <xaviergaray0010@gmail.com>
2023-12-17 21:56:08 -0500

    Added default cookie consent banner

--- alpha-corsi-website/pages/_app.js
+++ alpha-corsi-website/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/global.css';
+import CookieConsent from "react-cookie-consent";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+          <Component {...pageProps} />
+          <CookieConsent debug={true}>
+              <p>We collects your data in order to improve your experience in the form of cookies.</p>
+          </CookieConsent>
+      </>
+  );
 }
