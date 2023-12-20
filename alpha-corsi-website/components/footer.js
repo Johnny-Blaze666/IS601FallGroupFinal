5510fa5c34831af213132186ba6547300955505c
Xavier Garay <xaviergaray0010@gmail.com>
2023-12-20 12:16:17 -0500

    Corrected all footer links

--- alpha-corsi-website/components/footer.js
+++ alpha-corsi-website/components/footer.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from "./footer.module.css";
 import Link from "next/link";
 import SocialMedia from "./socialmedia";
@@ -5,26 +6,38 @@ import SocialMedia from "./socialmedia";
 
 
 export default function Footer({ opacity }) {
+    const [glow, setGlow] = useState(false);
+
+    const handleSocialClick = (e) => {
+        e.preventDefault();
+        setGlow(true);
+        setTimeout(() => setGlow(false), 1000); // Remove the glow after 1 second
+    };
+
+    const handleSupportClick = (e) => {
+        alert('Please submit your email address and someone from our customer support team will reach out to you for support');
+    }
+
     return (
         <div className={styles.footer} id={"footer"} style={{ '--opacity': opacity }}>
             <div className={styles.footerLinks}>
                 <Link href='/services'>Services</Link>
                 <Link href='/#contact-us'>Email</Link>
-                <Link href='/#contact-us'>Contact us</Link>
+                <Link href='/signup'>Contact us</Link>
                 <h2><Link href='/#'>ALPHACORSI</Link></h2>
                 <Link href='/about'>About</Link>
-                <Link href='/#'>Social</Link>
-                <Link href='/#'>Support</Link>
+                <Link href='/#socialmedia' onClick={handleSocialClick}>Social</Link>
+                <Link href='/#contact-us' onClick={handleSupportClick}>Support</Link>
             </div>
 
             <div className={styles.footerBorder}></div>
 
-            <div className={styles.socialLinks}>
-                <SocialMedia image='/images/companies/logos/blogger.svg' link='google' ariaLabel='Blogger'/>
-                <SocialMedia image='/images/companies/logos/instagram.svg' link='google' ariaLabel='Instagram'/>
-                <SocialMedia image='/images/companies/logos/x.svg' link='google' ariaLabel='X, formerly Twitter'/>
-                <SocialMedia image='/images/companies/logos/facebook.svg' link='google' ariaLabel='Facebook'/>
-                <SocialMedia image='/images/companies/logos/linkedin.svg' link='google' ariaLabel='LinkedIn'/>
+            <div className={styles.socialLinks} id="socialmedia">
+                <SocialMedia image='/images/companies/logos/blogger.svg' link='https://blogger.com' ariaLabel='Blogger' glow={glow}/>
+                <SocialMedia image='/images/companies/logos/instagram.svg' link='https://instagram.com' ariaLabel='Instagram' glow={glow}/>
+                <SocialMedia image='/images/companies/logos/x.svg' link='https://twitter.com' ariaLabel='X, formerly Twitter' glow={glow}/>
+                <SocialMedia image='/images/companies/logos/facebook.svg' link='https://facebook.com' ariaLabel='Facebook' glow={glow}/>
+                <SocialMedia image='/images/companies/logos/linkedin.svg' link='https://linkedin.com' ariaLabel='LinkedIn' glow={glow}/>
             </div>
 
             <p className={styles.trademark}>© 2023 AlphaCorsi</p>
